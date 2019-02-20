a3a270f649f71e29e883f61eba23119b35f197c7
p1409087 <christopher.page@etu.univ-lyon1.fr>
2019-02-20 14:35:27 +0100

    ajout de la classe vegtation

---
+++ include/Vegetation/Vegetation.h
@@ -0,0 +1,14 @@
+//
+// Created by chris on 20/02/19.
+//
+
+#ifndef PROJECTDOGE_VEGETATION_H
+#define PROJECTDOGE_VEGETATION_H
+
+
+class Vegetation {
+
+};
+
+
+#endif //PROJECTDOGE_VEGETATION_H
---
+++ src/Vegetation/Vegetation.cpp
@@ -0,0 +1,5 @@
+//
+// Created by chris on 20/02/19.
+//
+
+#include "../../include/Vegetation/Vegetation.h"
